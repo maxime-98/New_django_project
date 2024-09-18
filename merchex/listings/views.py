a2ecd25627422153a4341c15f28bd03622840ba5
NGNAMA <essotolom.ngnama@sap.com>
2024-09-18 11:10:51 +0200

    Add views hello, about, listings, contact and understand how views works

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,3 +1,15 @@
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def hello(request):
+    return HttpResponse('<h1>Hello Django!</h1>')
+
+def about(request):
+    return HttpResponse('<h1>A propos</h1> <p>Nous adorons merch !</p>')
+
+def listings(request):
+    return HttpResponse('<h1>Listings!</h1>')
+
+def contact(request):
+    return HttpResponse('<h1>Contact!</h1>')
